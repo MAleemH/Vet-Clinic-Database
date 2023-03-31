7df3f89e476a59de04671b7473e227ce4f926e0f
MAleemH <aleemhashmi4321@gmail.com>
2023-03-31 14:43:25 +0500

    Added queries in schema.sql

--- schema.sql
+++ schema.sql
@@ -32,3 +32,22 @@ ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCE
 
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (
+	vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE
+);
